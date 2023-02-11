623f8f0445bb3c858e0fba4d1e8ba869615261e5
vachtung-gigabidze <dmitry.zyadik@gmail.com>
2023-02-11 23:41:36 +0500

    fix di

---
+++ lib/app/di/init_di.config.dart
@@ -0,0 +1,50 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// InjectableConfigGenerator
+// **************************************************************************
+
+// ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:get_it/get_it.dart' as _i1;
+import 'package:injectable/injectable.dart' as _i2;
+import 'package:qlutter/app/data/dio_app_api.dart' as _i8;
+import 'package:qlutter/app/data/main_app_config.dart' as _i4;
+import 'package:qlutter/app/domain/app_api.dart' as _i7;
+import 'package:qlutter/app/domain/app_config.dart' as _i3;
+import 'package:qlutter/feature/auth/domain/auth_repository.dart' as _i6;
+import 'package:qlutter/feature/auth/domain/auth_state/auth_cubit.dart' as _i5;
+
+const String _prod = 'prod';
+const String _dev = 'dev';
+const String _test = 'test';
+
+/// ignore_for_file: unnecessary_lambdas
+/// ignore_for_file: lines_longer_than_80_chars
+extension GetItInjectableX on _i1.GetIt {
+  /// initializes the registration of main-scope dependencies inside of [GetIt]
+  _i1.GetIt init({
+    String? environment,
+    _i2.EnvironmentFilter? environmentFilter,
+  }) {
+    final gh = _i2.GetItHelper(
+      this,
+      environment,
+      environmentFilter,
+    );
+    gh.singleton<_i3.AppConfig>(
+      _i4.ProdAppConfig(),
+      registerFor: {_prod},
+    );
+    gh.singleton<_i3.AppConfig>(
+      _i4.DevAppConfig(),
+      registerFor: {_dev},
+    );
+    gh.singleton<_i3.AppConfig>(
+      _i4.TestAppConfig(),
+      registerFor: {_test},
+    );
+    gh.singleton<_i5.AuthCubit>(_i5.AuthCubit(gh<_i6.AuthRepository>()));
+    gh.singleton<_i7.AppApi>(_i8.DioAppApi(gh<_i3.AppConfig>()));
+    return this;
+  }
+}
