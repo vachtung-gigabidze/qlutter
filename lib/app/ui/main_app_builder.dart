119ccb34a4b2c8c3e15c1a4673f988d8b6749b5c
vachtung-gigabidze <dmitry.zyadik@gmail.com>
2023-02-15 23:01:16 +0500

    clean green

--- lib/app/ui/main_app_builder.dart
+++ lib/app/ui/main_app_builder.dart
@@ -1,9 +1,6 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
-import 'package:logging/logging.dart';
 import 'package:provider/provider.dart';
 import 'package:qlutter/app/di/init_di.dart';
 import 'package:qlutter/app/domain/app_builder.dart';
@@ -11,13 +8,10 @@ import 'package:qlutter/feature/auth/domain/auth_state/auth_cubit.dart';
 import 'package:qlutter/feature/internet_activity/internet_cubit.dart';
 import 'package:qlutter/feature/level_manager/level_manager.dart';
 
-// import 'game/ads/ads_controller.dart';
 import 'package:qlutter/feature/app_lifecycle/app_lifecycle.dart';
 import 'package:qlutter/feature/audio/audio_controller.dart';
-// import 'game/crashlytics/crashlytics.dart';
 import 'package:qlutter/feature/games_services/games_services.dart';
 import 'package:qlutter/feature/games_services/score.dart';
-// import 'game/in_app_purchase/in_app_purchase.dart';
 import 'package:qlutter/feature/level_selection/level_selection_screen.dart';
 import 'package:qlutter/feature/main_menu/main_menu_screen.dart';
 import 'package:qlutter/feature/play_session/play_session_screen.dart';
@@ -87,49 +81,7 @@ class MainAppBuilder implements AppBuilder {
           ]),
     ],
   );
-  // final _routes = GoRouter(
-  //   initialLocation: '/logo',
-  //   routes: [
-  //     GoRoute(
-  //         path: '/logo',
-  //         builder: (context, state) => const LogoScreen(nextRoute: '/')),
-  //     ShellRoute(
-  //         builder: (context, state, child) => ScaffoldWithBottomNavBar(
-  //               child: child,
-  //             ),
-  //         routes: [
-  //           GoRoute(
-  //               path: '/auth',
-  //               builder: (context, state) => AuthBuilder(
-  //                     isNotAuthorized: (context) => AuthScreen(),
-  //                     isWaiting: (context) => const AppLoader(),
-  //                     isAuthorized: (context, value, child) =>
-  //                         const ProfileScreen(),
-  //                   )),
-  //           GoRoute(
-  //               path: '/profile',
-  //               builder: (context, state) => AuthBuilder(
-  //                     isNotAuthorized: (context) => AuthScreen(),
-  //                     isWaiting: (context) => const AppLoader(),
-  //                     isAuthorized: (context, value, child) =>
-  //                         const ProfileScreen(),
-  //                   )),
-  //           GoRoute(
-  //               path: '/favorites',
-  //               builder: (context, state) => const FavoritesScreen()),
-  //           GoRoute(
-  //               name: "root",
-  //               path: '/',
-  //               builder: (context, state) => const RootScreen()),
-  //           GoRoute(
-  //               path: '/favorites',
-  //               builder: (context, state) => const FavoritesScreen()),
-  //           GoRoute(
-  //               path: '/freezer',
-  //               builder: (context, state) => const FreezerScreen()),
-  //         ])
-  //   ],
-  // );
+
   GamesServicesController? gamesServicesController;
 
   @override
@@ -214,9 +166,6 @@ class _GlobalProviders extends StatelessWidget {
           ),
           ProxyProvider2<SettingsController, ValueNotifier<AppLifecycleState>,
               AudioController>(
-            // Ensures that the AudioController is created on startup,
-            // and not "only when it's needed", as is default behavior.
-            // This way, music starts immediately.
             lazy: false,
             create: (context) => AudioController()..initialize(),
             update: (context, settings, lifecycleNotifier, audio) {
@@ -236,97 +185,3 @@ class _GlobalProviders extends StatelessWidget {
     );
   }
 }
-
-// Future<void> main() async {
-//   guardedMain();
-// }
-
-// void guardedMain() {
-//   if (kReleaseMode) {
-//     Logger.root.level = Level.WARNING;
-//   }
-//   Logger.root.onRecord.listen((record) {
-//     debugPrint('${record.level.name}: ${record.time}: '
-//         '${record.loggerName}: '
-//         '${record.message}');
-//   });
-
-//   WidgetsFlutterBinding.ensureInitialized();
-
-//   Logger _log = Logger('main.dart');
-
-//   _log.info('Going full screen');
-//   SystemChrome.setEnabledSystemUIMode(
-//     SystemUiMode.edgeToEdge,
-//   );
-
-  //GamesServicesController? gamesServicesController;
-
-  //LevelManager? levelManager = LevelManager()..readLevels();
-
-  // runApp(
-  //   Qlutter(
-  //     settingsPersistence: LocalStorageSettingsPersistence(),
-  //     playerProgressPersistence: LocalStoragePlayerProgressPersistence(),
-  //     gamesServicesController: gamesServicesController,
-  //     levelManager: levelManager,
-  //   ),
-  // );
-// }
-
-
-// class Qlutter extends StatelessWidget {
-  
-
-//   final PlayerProgressPersistence playerProgressPersistence;
-
-//   final SettingsPersistence settingsPersistence;
-
-//   final GamesServicesController? gamesServicesController;
-
-  
-
-//   final LevelManager levelManager;
-
-//   const Qlutter({
-//     required this.playerProgressPersistence,
-//     required this.settingsPersistence,    
-//     required this.gamesServicesController,
-//     required this.levelManager,
-//     super.key,
-//   });
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return AppLifecycleObserver(
-//       child: MultiBlocProvider(
-//         providers: [
-          
-//         ],
-//         child: Builder(builder: (context) {
-//           final palette = context.watch<Palette>();
-
-//           return MaterialApp.router(
-//             title: 'Qlutter',
-//             theme: ThemeData.from(
-//               colorScheme: ColorScheme.fromSeed(
-//                 seedColor: palette.darkPen,
-//                 background: palette.backgroundMain,
-//               ),
-//               textTheme: TextTheme(
-//                 bodyMedium: TextStyle(
-//                   color: palette.ink,
-//                 ),
-//               ),
-//               useMaterial3: true,
-//             ),
-//             routeInformationProvider: _router.routeInformationProvider,
-//             routeInformationParser: _router.routeInformationParser,
-//             routerDelegate: _router.routerDelegate,
-//             scaffoldMessengerKey: scaffoldMessengerKey,
-//           );
-//         }),
-//       ),
-//     );
-//   }
-// }
--- lib/feature/player_progress/persistence/local_storage_player_progress_persistence.dart
+++ lib/feature/player_progress/persistence/local_storage_player_progress_persistence.dart
@@ -1,13 +1,6 @@
-// Copyright 2022, the Flutter project authors. Please see the AUTHORS file
-// for details. All rights reserved. Use of this source code is governed by a
-// BSD-style license that can be found in the LICENSE file.
-
 import 'package:shared_preferences/shared_preferences.dart';
-
 import 'player_progress_persistence.dart';
 
-/// An implementation of [PlayerProgressPersistence] that uses
-/// `package:shared_preferences`.
 class LocalStoragePlayerProgressPersistence extends PlayerProgressPersistence {
   final Future<SharedPreferences> instanceFuture =
       SharedPreferences.getInstance();
