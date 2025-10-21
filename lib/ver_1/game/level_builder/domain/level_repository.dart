cf07233275c576d9e802a1b2f70fb5863120bc66
dimitrii <dmitry.zyadik@gmail.com>
2025-10-21 12:48:10 +0500

    delete ver1

--- lib/ver_1/game/level_builder/domain/level_repository.dart
+++
@@ -1,6 +0,0 @@
-abstract class LevelRepository {
-  Future<dynamic> getLevels();
-
-  Future<dynamic> sendProcess(
-      {int levelId, int steps, int seconds, DateTime dateTime});
-}
--- lib/ver_1/game/pages/pages.dart
+++
@@ -1,5 +0,0 @@
-export 'package:qlutter/ver_1/game/pages/level_select/page.dart';
-export 'package:qlutter/ver_1/game/pages/main_menu/page.dart';
-export 'package:qlutter/ver_1/game/pages/play_scene/page.dart';
-export 'package:qlutter/ver_1/game/pages/setting/page.dart';
-export 'package:qlutter/ver_1/game/pages/won/page.dart';
